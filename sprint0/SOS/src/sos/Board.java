af6b2dddfdc54c6cb76e2d82ad0e00c6c914f855
Tim DeGraffenreid <90429693+timdegraffenreid@users.noreply.github.com>
2023-03-11 16:39:05 -0600

    add Board class

---
+++ sprint0/SOS/src/sos/Board.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sos;
+
+/**
+ *
+ * @author Timd
+ */
+public class Board {
+    public Board(){
+        System.out.println("Board Creted");
+    }
+    public void msg(){
+        System.out.println("Message from Board");
+    }
+}
